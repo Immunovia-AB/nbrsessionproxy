00c9b47f989322bb31013b13c15ad2242ff4e483
Gunder Bitén <gunder.biten@gabcia.se>
2018-10-07 09:20:10 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="proxy",
+    version='0.1.0',
+    url="https://github.com/Immunovia-AB/proxy",
+    description="Jupyter extension to proxy RStudio's rsession",
+    packages=setuptools.find_packages(),
+	classifiers=['Framework :: Jupyter'],
+    install_requires=[
+        'notebook',
+        'nbserverproxy >= 0.8.3'
+    ],
+    package_data={'proxy': ['static/*']},
+)
